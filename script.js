9f842fb9cc629d4eaeee06fe3bd8b8651fe5a905
Rizky Anugrah <muhammadrizkyanugrah01@gmail.com>
2023-10-06 20:48:42 +0800

    Implemented BMI calculation and input reset function,then display result on html

---
+++ script.js
@@ -0,0 +1,36 @@
+// Function for Calculate BMI
+BMI_Calculate = () => {
+  const weightValue = document.getElementById("weight").value;
+  const heightValue = document.getElementById("height").value;
+
+  const calculation = weightValue / (heightValue / 100) ** 2;
+
+  let status;
+
+  switch (true) {
+    case calculation < 18.5:
+      status = "Underweight";
+      break;
+    case calculation >= 18.5 && calculation <= 24.9:
+      status = "Normal weight";
+      break;
+    case calculation >= 25 && calculation <= 29.9:
+      status = "Overweight";
+      break;
+    case calculation >= 30:
+      status = "Obesity";
+      break;
+    default:
+      status = "Invalid BMI";
+  }
+
+  const result = document.getElementById("result");
+  result.innerHTML = `BMI: ${calculation.toFixed(2)} - Status: ${status}`;
+};
+
+// Function for reset value
+resetValue = () => {
+  document.getElementById("weight").value = "";
+  document.getElementById("height").value = "";
+  document.getElementById("result").innerHTML = "";
+};
